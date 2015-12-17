f827275335d2565cf920c73708c76ad57e6c6f8e
centimani <frostmoth@gmail.com>
2015-12-17 15:15:37 -0600

    Update Project5.js

--- JS/Project5.js
+++ JS/Project5.js
@@ -5,24 +5,31 @@ var startLat = 38.883845;
 var startLng = -94.667995;
 var latLng = "{lat: %lat, lng: %lng}";
 
-var Model = {
-	//init: function(){
-	  //populate and build the marker Array which will be used to place markers
-	  rawData: function(){ //this is for points of interest
-	  	var MarkerArray = ko.observableArray([
+var BarkerArray = ko.observableArray([
 	   		{lat: "38.895180" , lng:"-94.669989", markerName:"HR HAVEN" },
 	   		{lat: "38.853392" , lng:"-94.682561", markerName:"Target" },
 	   		{lat: "38.885448" , lng:"-94.663601", markerName:"Mr Goodcents" },
 	   		{lat: "38.885366" , lng:"-94.688313", markerName:"Hy-Vee" },
 	   		{lat: "38.885676" , lng:"-94.699230", markerName:"Kindercare Switzer Commons"},
 	    ]);
-		//ko.applyBindings(new MarkerArray()); not sure if i need this, leave it heere just in case
-		return MarkerArray();
+	    
+ko.applyBindings(new BarkerArray());
+
+var Model = {
+	  
+	  //not sure if this should be in the model or the Map View. Decide Later
+	  markerData: function(){
+	    var markerObject = ko.observableArray([]);
+	    
+	    return markerObject;
 	  },
-	  searchArray: function(){
-	  	var localArray = Model.rawData();
-	  	var indexNumb = localArray.indexOf('markerName: HR HAVEN');
-	  	console.log(indexNumb);
+	  
+	  searchArray: function(searchValue){ //maybe we'll add another value ot pass into here for what element we're looking for IE lat,lng markerName for broader search functionality
+      for (var i = 0; i < BarkerArray().length; i++){
+        if (BarkerArray()[i].markerName == searchValue){ //sub target for searchValue function
+	  	    return i;
+        }
+      }
 	  },
 	  //other data generated from 3rd Party APIS
 };
@@ -37,32 +44,25 @@ var Model = {
 //knockout needs to handle List, Filter, and any other information subject to changing state.
 
 locationView = {
-
+  //populates the DropDown Menu
 	populateDropdown: function(){
-		//var dropDownBox = document.getElementById("selectBox"); //possibly move this to a global variable
-
-		var localArray = Model.rawData();
+		
 		var optionBase = '<option value="%value">%VisibleName</option>';
 
-		for (i = 0; i< localArray.length; i++){
-			var newOption = optionBase.replace("%value", localArray[i].markerName);
-				newOption = newOption.replace("%VisibleName", localArray[i].markerName);
+		for (i = 0; i< BarkerArray().length; i++){
+			var newOption = optionBase.replace("%value", BarkerArray()[i].markerName);
+				newOption = newOption.replace("%VisibleName", BarkerArray()[i].markerName);
 			$("#selectBox").append(newOption);
 		}
 	},
-	//this function will focus and zoom on the selected Drop Down Menu Option
+	
+	//returns the value of the Drop Down Menu
 	focusDropdown: function(){
 		var dropDownBox = document.getElementById("selectBox");
-		var ddSelection = dropDownBox.options[dropDownBox.selectedIndex].value; 
+		var ddSelection = dropDownBox.options[dropDownBox.selectedIndex].value;
+		console.log (ddSelection);
 		
-		//search the array
-		var arrayvalue= Model.rawData().indexOf('ddSelection');
-		console.log (arrayvalue);
-
-
-		var focusLocation = new google.maps.LatLng(38.885366, -94.688313); //best to wrap our values in this googlebox
-		Bigmap.setCenter(focusLocation);
-		Bigmap.setZoom(15);
+		return ddSelection;
 	},
 
 	pointOfInterestButton: function(){
@@ -75,11 +75,12 @@ locationView = {
 //additionall functionalit y to animate map markers
 //open information window from 3rd party APIS
 
+//TODO: Animate Selected Markers.
+    //  Show Information in the infowindow for each Marker
 mapView = {
 
 	init: function (){
 		
-
 		//initialize Map.
 		 var map = new google.maps.Map(document.getElementById('domMap'),{
 			center: {lat: startLat, lng: startLng},
@@ -87,8 +88,6 @@ mapView = {
 			zoom: 12
 		});
 		Bigmap=map; //set this new map we made to be used as a global variable
-    
-    	mapView.placeMapMarkers(startLat, startLng, "135th and Metcalf");
 
 		console.log("initilized");
 	},
@@ -109,6 +108,8 @@ mapView = {
 			title: mName,
 		  });
 		  
+		  //pass this marker into an array/obj
+		  
 		  //could seperate this out if we want complex pop ups
 		  var newInfoWin = new google.maps.InfoWindow({
 			content: mName, //this can be super complicated, its a full popup window
@@ -118,7 +119,13 @@ mapView = {
 	    newMarker.addListener('click', function(){
 	      newInfoWin.open(Bigmap, newMarker);
 	    });
+	    
+	    return newMarker;
     },
+
+  MapMarkerArray: function(){
+    
+  },
 
 	clearMapMarkers: function(){
 		//code to remove all map markers
@@ -129,29 +136,59 @@ mapView = {
 	popupMarker: function(){ //will pass in data in here from VM
 		//code to generate a popup over selected target
 	}
-}
+};
 
 	
 
  ViewModel= {
   
   //this one will grab info from the Model Array to build makers
-  //name can be changed later to avoid confusion
-  init: function(){
-  	//Grab info from the Model
-	var localArray = Model.rawData();
-    
+  initBuildMarkers: function(){
     //Loop through here and build in the MapView
-    for (i = 0; i < localArray.length; i++) {
-     	var locallat= localArray[i].lat;
-     	var locallng=localArray[i].lng;
-     	var localName = localArray[i].markerName;
+    for (i = 0; i < BarkerArray().length; i++) {
+     	var locallat= BarkerArray()[i].lat;
+     	var locallng=BarkerArray()[i].lng;
+     	var localName = BarkerArray()[i].markerName;
+     	
      	//we'll get name+ any other thing we need for markers here later
-     	mapView.placeMapMarkers(locallat, locallng, localName);
-
+     	
+     	mapView.placeMapMarkers(locallat, locallng, localName); //places Map Marker
+      
     }
   },
 
+//This function is focusing on a location we get, not a marker
+// this should perhaps be changed to get marker information
+  DropDownFocus: function(){
+    var focusValue = locationView.focusDropdown(); //returns the value we're going to search for
+    var focusIndex = Model.searchArray(focusValue); //searches for the index of the dropdown box is focused on
+    
+    var iLNG = BarkerArray()[focusIndex].lng; //gets the LNG value from index i
+    var iLAT = BarkerArray()[focusIndex].lat; //gets the LAT value from index i
+
+    var focusLocation = new google.maps.LatLng(iLAT, iLNG); //best to wrap our values in this googlebox
+		Bigmap.setCenter(focusLocation); //centers on focusLocation
+		Bigmap.setZoom(13); //sets Zoom
+		
+		//set marker and open
+		//newInfoWin.open(Bigmap, newMarker);
+  },
+  
+  init: function(){
+    ViewModel.initBuildMarkers();
+    locationView.populateDropdown();
+  }
+  
+  
  };
  
-//ViewModel.init();
+ 
+//Reminder: LISTENERS should not be used on Map.
+ Test ={
+   pushKO : function(){
+    BarkerArray().push('{lat: "39.04825" , lng:"-94.592884", markerName:"Boogers" }');
+    mapView.placeMapMarkers(39.04825,-94.592884,'Boogers'); //add the marker manually for now
+    console.log (BarkerArray().length);
+   }
+ };
+ 
