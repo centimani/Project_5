a2ea79fbc9ef6d2b732a9b95472ccfe50513fcb0
centimani <frostmoth@gmail.com>
2016-02-14 17:08:13 -0600

    Update Project5.js

--- JS/Project5.js
+++ JS/Project5.js
@@ -1,17 +1,10 @@
 //My Google Maps API Key:  AIzaSyACeu3Nlk0hCOlOHFX0bKVc5m2gFrcNTiQ
 
-//TODO: Largely 3rd party API interface + AJAX stuff
-//Under Test we have a function that'll place a new marker into our arrays. check to make sure it goes into ALL the arrays
-//We need to use the search bar to search through API calls
-//We also need to populate infoWindows, which again will largely be done with API calls.
-// Add a filter function, maybe make it a button that appears in the info window that'll remove itself from the arrays
-//also have a recent list, so that even if we delete from above, it'll remain on our recents tab
-
 var Bigmap= undefined; //used to save the Map reference
 var startLat = 38.883881; //used for init of Map
 var startLng = -94.667829; //used for init of Map
-var lastMarker = undefined; // this is used to toggle bounces
-var lastInfoWindow = undefined; //this is used to hold last Array
+var searchBox = document.getElementById("filter"); //searchBox Value
+var infoWindowsMade = 0; //used to make new infowindows pop up
 
 var BarkerArray = ko.observableArray([ //Holds the HardCoded Data I picked
 	   		{lat: "38.895180" , lng:"-94.669989", markerName:"HR HAVEN" },
@@ -22,33 +15,52 @@ var BarkerArray = ko.observableArray([ //Holds the HardCoded Data I picked
 	    ]);
 // all the functions are all modified at the same time, these will all share the same index in their respective Arrays, use this for searching
 var MapMarkerArray = ko.observableArray([]); //populated by their creation. This holds Markers
-//var MarkerNames = ko.observableArray([]);//populated by their creation. Currently Used by the DropDown Menu
 var infoWindowArray = ko.observableArray([]); //populated by their creation. This holds infoWindows
 
 var Model = { //Model will handle all of the calculations of the data
-	  searchArray: function(searchValue){ //add another value ot pass into here for what element for broader search functionality
-	  
-      for (var i = 0; i < MapMarkerArray().length; i++){
-        if (BarkerArray()[i].markerName == searchValue){ //sub target for searchValue function
-	  	    return i;
-        }
-      }
-	  },
-	  //other data generated from 3rd Party APIS
 };
-
-//locations View
-//Search and Filter Locations here
-//Text area here to filter markers that already show up
-//Search function will not be enough through 3rd party API, must filter myself
-//knockout needs to handle List, Filter, and any other information subject to changing state.
 
 locationView = {
-  //new location functions here
-};
 
-// Maps View
-//open information window from 3rd party APIS
+  activateSearchBox: function(){
+    var SearchValue=searchBox.value; //gets the value of the Search Box
+    
+    if(SearchValue.length !== 0){ //if the search box is empty or full
+      locationView.searchLocations(SearchValue); //search for stuff if the box has a value in it
+    }
+    else{ //else reset all markers
+     ViewModel.showAllMarkers(); //shows all Markers
+     mapView.closeAllInfoWindows();// closes all InfoWindows
+     ViewModel.markerUnBounceAll(); //unbounces all Markers
+    }
+  },
+  
+  searchLocations: function(searchValue){
+    for (var i = 0; i < MapMarkerArray().length; i++){ //for every marker in the marker array
+      var markerTitle = MapMarkerArray()[i].title; //this is the value we're searching against
+      var searchValueLength = searchValue.length;
+    //we're going to slice the title based on the length of the searchValue
+      locationView.wordSearch(markerTitle,searchValue,searchValueLength, MapMarkerArray()[i]); //this does a comparison between two values@index and inclues marker.
+    }
+  },
+  
+  wordSearch: function(MarkerTitle,SearchedWord,letterIndex,marker){
+    var mTitleLC = MarkerTitle.toLowerCase();//moves the Marker Title to LowerCase
+    var sWordLC = SearchedWord.toLowerCase();//moves the Searched Word to LowerCase
+    var slicedTitle = mTitleLC.slice(0,letterIndex); //cuts up the Marker end for comparision
+    var slicedSearch = sWordLC.slice(0,letterIndex); //custs up the search for comparasion
+    
+    if(slicedTitle == slicedSearch){
+      ViewModel.markerBounce(marker); //make this marker bounce
+      var markerIndex = MapMarkerArray().indexOf(marker); //get the index value of our marker
+      mapView.openOneMarkerInfoWindow(markerIndex); //open that InfoWindow
+    }
+    else if(slicedTitle != slicedSearch){
+      ViewModel.unBounce(marker); //Unbounce marker
+      ViewModel.hideMarker(marker); //hide marker
+    }
+  },
+};
 
 mapView = {
   
@@ -59,91 +71,131 @@ mapView = {
 			zoom: 13
 		});
 		Bigmap=map; //set this new map we made to be used as a global variable
-	  mapView.initSearchBox();
-	  
-		console.log("initilized");
+	  mapView.initSearchBox();//inits the search box
 	},
 	
 	initSearchBox: function(){
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    //Bigmap.controls[google.maps.ControlPosition.TOP_LEFT].push(input); //moves the search box into the Map Window
-
+    var input = document.getElementById('pac-input'); //saves the search box to a variable
+    var searchBox = new google.maps.places.SearchBox(input); // initilizes searchbox as a google search box
+    Bigmap.controls[google.maps.ControlPosition.TOP_CENTER].push(input); //moves the search box into the Map Window
     // Bias the SearchBox results towards current map's viewport.
     Bigmap.addListener('bounds_changed', function() {
       searchBox.setBounds(Bigmap.getBounds());
     });
 
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
+  searchBox.addListener('places_changed', function() { //listener to retrieve details of searched place
     var places = searchBox.getPlaces();
 
     if (places.length ===  0) {
       return;
     }
     //get the selected search option and places it on the map
-    var selectedLat =(places[0].geometry.location.lat());
-    var selectedLng = (places[0].geometry.location.lng());
-    var selectedName = (places[0].name);
+    var selectedLat =(places[0].geometry.location.lat()); //get lat
+    var selectedLng = (places[0].geometry.location.lng()); //get lng
+    var selectedName = (places[0].name); //get name
     mapView.placeMapMarkers(selectedLat,selectedLng,selectedName);//places marker
-    //BarkerArray().push({lat: selectedLat , lng: selectedLng, markerName:selectedName });//this is redudent, but breaks the dropdown focus function, fix later
     mapView.resizeMap(); //resize map
     var NewFocus=MapMarkerArray().length - 1; //get the last index
     var newMarker=MapMarkerArray()[NewFocus]; //get recently added marker
-    ViewModel.markerBounce(newMarker); //make it bouuunce
-    
-    
+    ViewModel.markerBounceAll(newMarker); //make it bouuunce
     });
 	},
 
   resizeMap: function(){
     var bounds = new google.maps.LatLngBounds();
-    for(i=0;i<MapMarkerArray().length;i++) {
+    for(var i=0; i < MapMarkerArray().length; i++) { //adds each marker to a greater bounding function
       bounds.extend(MapMarkerArray()[i].getPosition());
     }
-  Bigmap.fitBounds(bounds);
+  Bigmap.fitBounds(bounds); //resizes map
   },
 	
 	placeMapMarkers: function(lat, lng, mName){
-     
 		var myLatlng = new google.maps.LatLng(lat, lng); //way to save in LatLng values so google likes it
     var newMarker = new google.maps.Marker({
-			position: myLatlng, //VMlat, VMlng from Model
-			map: Bigmap,
+			position: myLatlng, //use our newly created latlng
+			map: Bigmap, //sets it to our map
 			animation: google.maps.Animation.DROP,//sets drop animation when it builds
 			title: mName,
 		  });
 		  
 		  MapMarkerArray.push(newMarker);//passes this data into the MarkerArray
-		  //MarkerNames.push(mName); //passes this data into the MarkerNames.Unsure about this one.
-		  
-		  ///This needs to be expanded and moved out to a seperate function to create a new InfoWindow
-		  var newInfoWin = new google.maps.InfoWindow({
-			content: mName, //this can be super complicated, its a full popup window
-		  });
-		  ///////////////////////////////////////////////////////////
-	    
-	    infoWindowArray.push(newInfoWin);
-	    
-	    newMarker.addListener('click', function(){ //Adds ClickEvent to Button
-	      newInfoWin.open(Bigmap, newMarker);
+		  var newIndex = MapMarkerArray().indexOf(newMarker);
+
+      newMarker.addListener('click', function(){ //Adds ClickEvent to Button
+        var myInfoWindow= infoWindowArray()[newIndex];
+	      myInfoWindow.open(Bigmap, newMarker);
+	      ViewModel.markerBounceAll(newMarker); //should bounce when its clicked too
+	      var newMarkerIndex = MapMarkerArray().indexOf(newMarker);
+	      mapView.openMarkerInfoWindow(newMarkerIndex); //Highlights infoWindow. I think this is where we're erring
 	    });
 	    
-	    return newMarker;
+	    mapView.buildInfoWindow(newMarker); //builds info window using our current marker information
+	    return newMarker; //returns the new marker. dont' remember if I'm still using this
     },
-
-  openMarkerInfoWindow: function(myIndex){
-    var currentInfoWindow = infoWindowArray()[myIndex]; //gets the Info Window at the index
-    var currentMarker = MapMarkerArray()[myIndex]; //gets the Map Marker at the index
-    currentInfoWindow.open(Bigmap,currentMarker); //opens the infoWindo
+  
+  buildInfoWindow: function(passedMarker){
+      var myLat = passedMarker.position.lat(); //set lat
+      var myLng = passedMarker.position.lng(); //set lng
+      var myName = passedMarker.title; //set title
+      var markerIndex = MapMarkerArray().indexOf(passedMarker); //get index of passed marker
+      mapView.retrieveData(myLat,myLng,myName, markerIndex); //use all this to to make the AJAX call
   },
 
-	clearMapMarkers: function(){
-		//code to remove all map markers
-		//https://developers.google.com/maps/documentation/javascript/examples/marker-remove
-		//clearMarkers();
-		//empty the array
+  retrieveData: function(lat,lng, name, index){
+  var myClientID = "3MCVEAWYXO4UL1RPEYO1XBIXQPNGEYXITVOD2X3EF5E0LT3W";
+  var mySecretID = "FZFTDA0MHSVNUCDYYZXUCELLQNKLO2NIDGIXIZPNASPRNU0M";
+  var ajaxURL = "https://api.foursquare.com/v2/venues/search?client_id="+myClientID+"&client_secret="+mySecretID+"&v=20130815&ll="+lat+","+lng+"&query="+name;
+   
+  $.get(ajaxURL ,function(data,status){
+      //might do a confirmation that verified==true;
+      var infoName = data.response.venues[0].name; //returns company name
+      var infoPhone = data.response.venues[0].contact.formattedPhone; //returns a phone number
+      var infoURL = data.response.venues[0].url; //returns the URL
+      var infoBuilding = data.response.venues[0].categories[0].name; //returns the type of building
+      var infoHereNow = data.response.venues[0].hereNow.summary; //returns if they're open
+      var infoAddress = data.response.venues[0].location.address; //returns physical address
+      var infoTwitter = data.response.venues[0].contact.twitter; //returns twitter acct name
+      var infoCheckin = data.response.venues[0].stats.checkinsCount;//returns the checkins count
+      mapView.makeInfoWindow(infoName,infoPhone,infoURL,infoBuilding,infoHereNow,infoAddress,infoTwitter,infoCheckin,index);
+    })
+      .fail(function() {
+        alert("Couldn't retrieve data."); //lolfail
+      });
+  },
+  
+  makeInfoWindow: function(name,phone,url,buildingtype,herenow,address,twitter,checkin,insertIndex){
+    var infoWindowBase="<span class='bTitle'>"+name+"</span><br><span>"+buildingtype+"</span>"+" - "+"<span>"+herenow+"</span><br><span>"+address+"</span><br><span>"+phone+"</span><br><span>Website: </span><a href='" +url+"'>"+name+"</a>"+",  "+"<span>Twitter: </span><a href='http://twitter.com/"+twitter+"'>"+twitter+"</a><br>"; //base for what the infoWindows will contain
+    var newInfoWin = new google.maps.InfoWindow({
+			content: infoWindowBase, //uses the base to make this new infowindow
+		  });
+    infoWindowArray.splice(insertIndex,0,newInfoWin); //adds the new info window to the array to match it's marker since they don't all process at the same time
+    var newIndex= infoWindowArray().indexOf(newInfoWin);//index of the new info window
+    infoWindowsMade++; //add one to keep the Barker Array markers from opening initially, but each added one would after
+  
+    if(infoWindowsMade > 5){//if this new infoWindow was not made from the barker array
+    mapView.openMarkerInfoWindow(newIndex); //opens the new infoWindow after the initial infoWindows are made
+    }
+  },
+  
+  openMarkerInfoWindow: function(myIndex){
+    var currentMarker = MapMarkerArray()[myIndex]; //gets the Map Marker at the index
+    var myInfoWindow= infoWindowArray()[myIndex]; //gets the Info Window at the index
+    for (i = 0; i < infoWindowArray().length; i++){ //closes ALL the infoWindows
+      infoWindowArray()[i].close();
+    }
+    myInfoWindow.open(Bigmap,currentMarker); //opens the infoWindow
+  },
+  
+  openOneMarkerInfoWindow: function(myIndex){ //this is for multiple infoWindows
+     var currentInfoWindow = infoWindowArray()[myIndex]; //gets the infoWindow at the Index
+     var currentMarker = MapMarkerArray()[myIndex]; //gets the Map Marker at the index
+     currentInfoWindow.open(Bigmap,currentMarker); //opens the infoWindow
+  },
+
+	closeAllInfoWindows: function(){
+	 for (i = 0; i < infoWindowArray().length; i++){ //closes ALL the infoWindows
+      infoWindowArray()[i].close();
+	 }
 	},
 };
 
@@ -154,95 +206,65 @@ mapView = {
      	var locallat= BarkerArray()[i].lat;
      	var locallng=BarkerArray()[i].lng;
      	var localName = BarkerArray()[i].markerName;
-     	
-     	//we'll get name+ any other thing we need for markers here later
      	mapView.placeMapMarkers(locallat, locallng, localName); //places Map Marker
-      
-    }
-  },
-
-  DropDownFocus: function(){ //Centers/Zooms/Bounces selected Marker from the Dropdown
-    var focusValue = locationView.valueDropdown(); //returns the value we're going to search for
-    var focusIndex = Model.searchArray(focusValue); //searches for the index of the dropdown box is focused on
-    var iLNG = MapMarkerArray()[focusIndex].lng; //gets the LNG value from index i
-    var iLAT = MapMarkerArray()[focusIndex].lat; //gets the LAT value from index i
-
-    var focusLocation = new google.maps.LatLng(iLAT, iLNG); //best to wrap our values in this googlebox
-		//Bigmap.setCenter(focusLocation); //centers on focusLocation
-		Bigmap.setZoom(13); //sets Zoom
-		
-		if (infoWindowArray().length > 0){ //all this needs infoWindowArray to already be populated
-		  mapView.openMarkerInfoWindow(focusIndex);
-		  var myMarker=MapMarkerArray()[focusIndex]; //gets the marker at the focused Index
-		  ViewModel.markerBounce(lastMarker); //turns off the last marker bounce
-		  lastInfoWindow.close(); //closes the last infoWindow
-		  ViewModel.markerBounce(myMarker); //makes this marker bounce
-		  lastMarker=MapMarkerArray()[focusIndex]; //sets the new LastMarker to this one
-		  lastInfoWindow=infoWindowArray()[focusIndex]; //sets the lastInfoWindow to this one
-		  mapView.resizeMap();
-		}
-		
-  },
-  
-   Focus: function(indexValue){ //Bounces selected Marker and opens infoWindow
-		//console.log($index);
-		mapView.openMarkerInfoWindow(indexValue);
-		var myMarker=MapMarkerArray()[indexValue]; //gets the marker at the focused Index
-		ViewModel.markerBounce(lastMarker); //turns off the last marker bounce
-		lastInfoWindow.close(); //closes the last infoWindow
-		ViewModel.markerBounce(myMarker); //makes this marker bounce
-		lastMarker=MapMarkerArray()[indexValue]; //sets the new LastMarker to this one
-		lastInfoWindow=infoWindowArray()[indexValue]; //sets the lastInfoWindow to this one
-		mapView.resizeMap();
-  },
-  markerBounce: function(passMarker){ //Toggles Bounce of a Map Marker
-    if (passMarker.getAnimation() !== null){ //Bounce: Off
-      passMarker.setAnimation(null);
-    }
-    else {
-      passMarker.setAnimation(google.maps.Animation.BOUNCE); //Bounce:On
     }
   },
   
-  init: function(){ //initializes all the things
+  Focus: function(focusedMarker){ //Bounces selected Marker and opens infoWindow focusedMarker
+    var focusedIndex = MapMarkerArray.indexOf(focusedMarker);
+		mapView.openMarkerInfoWindow(focusedIndex); //open map Marker info window
+		var myMarker=MapMarkerArray()[focusedIndex]; //gets the marker at the focused Index
+		ViewModel.markerBounceAll(myMarker); //makes this marker bounce
+		mapView.resizeMap(); //resize map
+  },
+  
+  markerBounceAll: function(passMarker){
+    for (i = 0; i < MapMarkerArray().length; i++) {
+      MapMarkerArray()[i].setAnimation(null); //Turns off the Bounce in the entire Marker Array
+    }
+    passMarker.setAnimation(google.maps.Animation.BOUNCE); //Turns Bounce on one we want to bounce.
+  },
+  
+  markerUnBounceAll: function(){
+    for (i = 0; i < MapMarkerArray().length; i++) {
+      MapMarkerArray()[i].setAnimation(null); //Turns off the Bounce in the entire Marker Array
+    }
+  },
+  
+  markerBounce: function(passMarker){
+    passMarker.setAnimation(google.maps.Animation.BOUNCE); //make a specific marker bounce
+  },
+  
+  unBounce: function(passMarker){
+    passMarker.setAnimation(null); //turns off a specific map marker
+  },
+  
+  init: function(){ //initializes all the Map Markers
     ViewModel.initBuildMarkers();
-    //locationView.populateDropdown();
-    lastMarker=MapMarkerArray()[0]; //sets the lastMarker to the intitial Value
-    lastMarker.setAnimation(google.maps.Animation.BOUNCE); //fixes our first item not bouncing
-    lastInfoWindow = infoWindowArray()[0]; //sets our lastInfoWindow To Zero Index
-    //openMarkerInfoWindow(0); //opens our starter infoWindow
   },
   
-  populateInfoWindows: function(){
-    //nuff said
-  },
-  removeMarker: function(){
-    //this is where we remove a marker from the map
+  removeMarker: function(markerToRemove){
+    var removedIndex = MapMarkerArray.indexOf(markerToRemove); ///get the index of the marker to be removed
+    MapMarkerArray.remove(markerToRemove); //removes this marker from the array
+    markerToRemove.setMap(null); //removes them from the map
+    infoWindowArray.splice(removedIndex, 1);
+    mapView.resizeMap();
   },
   
+  hideMarker: function(markerToHide){
+    markerToHide.setMap(null); //hides the marker w/o removing from the array
+  },
   
+  showMarker: function(markerToShow){
+    markerToShow.setMap(Bigmap); //shows the marker that was hidden
+  },
+  
+  showAllMarkers: function(){
+    for (var i = 0; i < MapMarkerArray().length; i++){
+      MapMarkerArray()[i].setMap(Bigmap); //turns ALL markers on
+    }
+  },
  };
- 
-//This is garbage town
- Test ={
-   //Function adds a marker at the specified location, and centers it
-   pushKO : function(myLat, myLng, myName){
-    
-    myLat=39.04825;
-    myLng=-94.592884;
-    myName="Boogers";
-     
-    BarkerArray().push({lat: myLat , lng: myLng, markerName:myName }); //adds marker info to the marker
-    mapView.placeMapMarkers(myLat,myLng, myName); //add the marker manually for now
-    
-    //center, maybe seperate this out or put it directly into the map view
-    var focusLocation = new google.maps.LatLng(myLat, myLng); //best to wrap our values in this googlebox
-		Bigmap.setCenter(focusLocation); //centers on focusLocation
-		Bigmap.setZoom(13); //sets Zoom
-    
-   },
-   
- };
- 
+
  ko.applyBindings();
 
